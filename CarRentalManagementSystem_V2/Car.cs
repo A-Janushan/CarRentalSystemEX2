5afc43342e78dcbf2d61454d6fad6db1214f4df3
Janushan Anantharasa <88024116+A-Janushan@users.noreply.github.com>
2024-10-11 16:33:32 +0530

    Add files via upload

---
+++ CarRentalManagementSystem_V2/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem_V2
+{
+    internal class Car
+    {
+        public string ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal RentalPrice { get; set; }
+
+    }
+
+        
+    }
+}
